2541a66474b83ef3ae98b0afdbb7b4c4f41fd634
Jacob Rivera <edjacob25@gmail.com>
2023-09-03 19:04:59 -0700

    Added save function

--- functions/api/points.ts
+++ functions/api/points.ts
@@ -2,10 +2,11 @@ import {DateTime} from "luxon";
 
 interface Env {
     APIKEY: string;
+    SAVE_APIKEY: string;
     LOC_DB: D1Database;
 }
 
-export const onRequest: PagesFunction<Env> = async (ctx) => {
+export const onRequestGet: PagesFunction<Env> = async (ctx) => {
 
     let apikey = ctx.request.headers.get("apikey");
     if (apikey != ctx.env.APIKEY) {
@@ -18,4 +19,21 @@ export const onRequest: PagesFunction<Env> = async (ctx) => {
         return Response.json(data.results);
     }
     return Response.json("");
+}
+
+export const onRequestPost: PagesFunction<Env> = async (ctx) => {
+
+    let apikey = ctx.request.headers.get("apikey");
+    if (apikey != ctx.env.SAVE_APIKEY) {
+        return new Response("", {status: 401});
+    }
+
+    let data: {lat: number, lon: number} = await ctx.request.json();
+    let dt = DateTime.utc();
+    let stmt = ctx.env.LOC_DB.prepare("INSERT INTO location VALUES (?, ?, ?, ?)").bind(data.lat, data.lon, dt.toString(), dt.setZone("Asia/Tokyo").toString());
+    let res = await stmt.run();
+    if (res.success) {
+        return new Response();
+    }
+    return new Response("", {status: 400});
 }
